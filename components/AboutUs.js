d85580edb68de60a7624869bce11defeaf60637d
JSTop1Coder <jstopcoder0209@gmail.com>
2023-11-24 22:22:58 +0900

    update about-us style

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -5,7 +5,7 @@ const AboutUs = () => {
                 <div className="container-body">
                     <div className="container-body-text">
                         <h1 className="title pb-20">About Us</h1>
-                        <p className="content">
+                        <div className="content">
                             <div className="content-title">
                                 <h3>Join the Codo Ecosystem: Invest, Stake, and Grow</h3>
                             </div>
@@ -13,7 +13,7 @@ const AboutUs = () => {
                                 Codo Finance is an innovative Launchpad that connects its community with high-potential projects in various industries such as memes, gaming, and AI.
                                 Hurry to be part of our ecosystem where our users will experience the ease of earning while learning with DeFi and AI tools. Taake your place by buying tokens now and earn more by staking your purchased tokens.
                             </div>
-                        </p>
+                        </div>
                     </div>
                     <div className="container-body-img">
                         <img src="/assets/images/evreka.png" width={680} className=" m-auto"/>
